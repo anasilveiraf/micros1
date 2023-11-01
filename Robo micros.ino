3c312c36d041ec4f455459b226f0a92b233c732a
anasilveiraf <51797697+anasilveiraf@users.noreply.github.com>
2023-11-01 20:26:47 -0300

    cruz

--- Robo micros.ino
+++ Robo micros.ino
@@ -5,12 +5,14 @@ int roda_esquerda2 = 9; // azul in2
 int enable_2 = 6; // roxo
 int roda_direita1 = 5; // verde in3
 int roda_direita2 = 4; // azul in4
-int detector_direita = 2;
-int detector_esquerda = 3;
+int detector_direita = 7;
+int detector_esquerda = 8;
 volatile byte state = LOW;
-
 int detecta_direita;
 int detecta_esquerda;
+
+  
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,15 +25,17 @@ void setup() {
   pinMode(roda_direita1, OUTPUT);
   pinMode(roda_direita2, OUTPUT);
 
-  pinMode(detector_direita, INPUT);
-  pinMode(detector_esquerda, INPUT);
+ /* pinMode(detector_direita, INPUT_PULLUP);
+  pinMode(detector_esquerda, INPUT_PULLUP);
   
+  attachInterrupt(digitalPinToInterrupt(detector_direita), blink, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(detector_esquerda), blink, CHANGE);
 }
 
 void blink() {
   state = !state;
+}*/
 }
-
 void ir_para_frente ( ){
 
   digitalWrite(roda_esquerda1,HIGH);
@@ -61,28 +65,30 @@ void parar ( ){
 
 void ir_para_direita ( ){
 // delay 945ms para girar totalmente
+  while (detecta_esquerda == LOW){
   digitalWrite(roda_esquerda1,HIGH);
   digitalWrite(roda_esquerda2,LOW);
   digitalWrite(roda_direita1,LOW);
-  digitalWrite(roda_direita2,HIGH);
-
+  digitalWrite(roda_direita2,HIGH);}
 }
+
+
 
 void ir_para_esquerda ( ){
 // delay delay(1030); para girar totalmente
+  while(detecta_direita == LOW){}
   digitalWrite(roda_esquerda1,LOW);
   digitalWrite(roda_esquerda2,HIGH);
   digitalWrite(roda_direita1,HIGH);
-  digitalWrite(roda_direita2,LOW);
+  digitalWrite(roda_direita2,LOW);}
+
+void cruz ( ){
+// delay delay(1030); para girar totalmente
+  delay(10);
 }
 
 void detector_de_linha( ){
-  int detecta_direita;
-  int detecta_esquerda;
-
-  detecta_direita = digitalRead(detector_direita);
-  detecta_esquerda = digitalRead(detector_esquerda);
-
+ 
   if(detecta_direita == LOW) 
   {
     ir_para_esquerda ( );
@@ -99,29 +105,27 @@ void loop() {
 
   analogWrite(enable_2, 200);
   analogWrite(enable_1, 200);
-  
-  detecta_direita = digitalRead(detector_direita);
+
+detecta_direita = digitalRead(detector_direita);
   detecta_esquerda = digitalRead(detector_esquerda);
   
-  while(detecta_direita == detecta_esquerda) 
+  while(detecta_direita == detecta_esquerda == LOW) 
   {
-    ir_para_frente ( );
-    detecta_direita = digitalRead(detector_direita);
-  	detecta_esquerda = digitalRead(detector_esquerda);
-    Serial.println(detecta_direita);
+    ir_para_frente ( );  
   }
 
-/*  if(detecta_direita == LOW) 
-  {
-    ir_para_esquerda ( );
-    
-  }
-  if(detecta_esquerda == LOW) 
-  {
+if (detecta_direita == detecta_esquerda == HIGH)
+ cruz();
+else 
+  if (detecta_direita == LOW){}
     ir_para_direita ( );
-  }*/
+   if (detecta_esquerda == LOW)
+    ir_para_esquerda ( );
 
   delay(100);
+  //
+  
+
 
 }
 
